725473b8f73cdcd3ff1d21082002e159ef626ed2
ahmad-kashkoush <ahmedkashkoush464@gmail.com>
2024-02-15 14:14:35 +0200

    Chore: Update Repo

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import './../Styles/App.css';
+import './../Styles/Sidbar.css';
+import { Sidebar } from './Sidebar';
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <div>content</div>
+    </div>
+  );
+}
+export default App;
